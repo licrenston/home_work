8c457cee5a6c7cef2919d85309eb2c2c01efaeb2
licrenston <bserhiy@gmail.com>
2022-10-16 08:27:17 +0300

    hw3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
+import {getUsers} from "./services/user.service";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+        getUsers().then(value => console.log(value));
+    },[])
+    return (
+        <div>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/services/user.service.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+let axiosInstance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com',
+    headers: {}
+});
+
+const getUsers = () => {
+  return axiosInstance.get('/users');
+}
+
+export {getUsers};
